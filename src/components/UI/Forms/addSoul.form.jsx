52a577ae17b793e080b64898bdb90ab5bca98527
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-09-10 13:30:48 +0100

    fix: validation bug (#28)
    * slight improvements on the ui for begin registration page
    
    * removed required validation for surname and added bottom margin
    
    * add dist folder to git ignore
    
    * added operation 5s logo
    
    * updated to operation 5s logo
    
    * fixed number validation bug
    
    * [souls/workers] to souls

--- src/components/UI/Forms/addSoul.form.jsx
+++ src/components/UI/Forms/addSoul.form.jsx
@@ -15,8 +15,8 @@ export default function AddSoulsFormControl() {
         </div>
         <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 w-full mb-6 group">
-                <input style={{color: '#6B6B6B'}} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-gray-900 text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="floating_phone" class="peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (+23-456-7890)</label>
+                <input style={{color: '#6B6B6B'}} type="tel" pattern="^\d{11}$" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-gray-900 text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label for="floating_phone" class="peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (08012345678)</label>
             </div>
             <div class="relative z-0 w-full mb-6 group">
             <input style={{color: '#6B6B6B'}} type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-gray-900 text-base bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -38,7 +38,7 @@ export default function AddSoulsFormControl() {
             <select style={{color: '#6B6B6B6'}} id="large" class="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 ">
             <option selected>Select Gender</option>
             <option value="MA">Male</option>
-            <option value="FE">Femail</option>
+            <option value="FE">Female</option>
             </select>
         </div>
         <div class="relative z-0 w-full mb-6 group">
