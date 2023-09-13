7d5063a6ceb54be9f21870068b0bffb9081c6434
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-09-13 16:02:27 +0000

    updated table title in admin page

--- src/components/Table/admins.table.jsx
+++ src/components/Table/admins.table.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import TransitionsModal from '../ModalPopup/modalTransition'
-import AddSoulsFormControl from '../UI/Forms/addSoul.form'
+import AddAdminFormControl from '../UI/Forms/addAdmin.form'
 import SearchBox from '../Searchbox/searchbox';
 import ReusableTable from './Table.reusable';
 import PaginationFooter from '../PaginationFooter';
@@ -31,17 +31,17 @@ export default function AdminTables() {
         <div className="px-4 sm:px-6 lg:px-8 bg-white py-7">
           <div className="sm:flex sm:items-center">
             <div className="sm:flex-auto">
-              <h1 className="text-base font-semibold leading-6 text-gray-900">Admins</h1>
+              <h1 className="text-2xl font-semibold leading-6 text-gray-900">Admins</h1>
               <p className="mt-2 text-sm text-gray-700">
-                A list of all the souls in your account.
+                A list of all Opeation 5s admins .
               </p>
             </div>
             <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
               <button
                 className="block rounded-md px-3 bg-[#Bf0A30] py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#38404b] delay-100 ease-in-out duration-300 p-6"
               > 
-                <TransitionsModal name={'+ Add Soul'} heading={'Add new Soul Form'} width={'max-w-6xl w-[90%]'}>
-                  <AddSoulsFormControl />
+                <TransitionsModal name={'+ Add Admin'} heading={'Add a new admin'} width={'max-w-6xl w-[90%]'}>
+                  <AddAdminFormControl />
                 </TransitionsModal>
               </button>
             </div>
