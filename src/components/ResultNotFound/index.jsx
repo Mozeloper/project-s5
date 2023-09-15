66a0c90c111c7c2e77f4e57d87b320ad8cc1ca38
king danie <daniedemola@gmail.com>
2023-09-15 15:27:05 +0100

    addded result not found component

---
+++ src/components/ResultNotFound/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReturnToPrevious from './../ReturnToPrevious';
+import NotFoundImg from '../../assets/not-found.jpg';
+
+export default function ResultNotFound() {
+  return (
+    <>
+      <div className=" bg-white p-5">
+        <ReturnToPrevious />
+        <div className="flex flex-col items-center justify-center h-[70vh] text-center">
+          <img src={NotFoundImg} className="w-[200px]" />
+          <h1 className="text-3xl mt-4"> Record Not Found</h1>
+          {/* <p className="text-xl text-primary mb-8">No record was found.</p> */}
+        </div>
+      </div>
+    </>
+  );
+}
