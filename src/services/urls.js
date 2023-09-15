ceced03b73fc3d9b0bbf5b36296548703a1c214a
king danie <daniedemola@gmail.com>
2023-09-15 15:27:41 +0100

    fixed typo in get worker details endpoint

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,46 +1,46 @@
 export const appUrls = {
   //Authentication
-  LOGIN_URL: "/api/v1/Authentication/login",
-  REGISTER: "/api/v1/Authentication/register",
-  REFRESHTOKEN_URL: "/api/v1/Authentication/refresh-token",
-  FORGETPASSWORD_URL: "/api/v1/Authentication/forget-password",
-  RESETPASSWORD_URL: "/api/v1/Authentication/reset-password",
+  LOGIN_URL: '/api/v1/Authentication/login',
+  REGISTER: '/api/v1/Authentication/register',
+  REFRESHTOKEN_URL: '/api/v1/Authentication/refresh-token',
+  FORGETPASSWORD_URL: '/api/v1/Authentication/forget-password',
+  RESETPASSWORD_URL: '/api/v1/Authentication/reset-password',
 
   //Department
-  GETCHURCHDEPT: "/api/Entity/get-church-department",
+  GETCHURCHDEPT: '/api/Entity/get-church-department',
 
   //Super Admins
-  GETALLWORKERS_URL: "/api/SuperAdmin/get-all-workers", //super
-  GET_ALL_New_Converts_URL: "/api/SuperAdmin/get-all-new-converts",
-  GET_WORKER_DETAILS: "api/SuperAdmin/get-new-converts-by-worker-id", //get worker by id
+  GETALLWORKERS: '/api/SuperAdmin/get-all-workers', //super
+  GET_ALL_New_Converts_URL: '/api/SuperAdmin/get-all-new-converts',
+  GET_WORKER_DETAILS: 'api/SuperAdmin/get-worker', //get worker by id
 
   //Worker
-  GETSINGLEWORKERDETAILS_URL: "/api/SuperAdmin/get-all-workers", //worker
+  GETSINGLEWORKERDETAILS_URL: '/api/Worker/get-worker', //worker
 
   //New converts (soul)
   GET_ALL_New_Convert:
-    "api/NewConvertAdmin/get-all-new-convants-for-newconvertadmin",
+    'api/NewConvertAdmin/get-all-new-convants-for-newconvertadmin',
 
   //Ministry
   GET_ALL_MINISTRY_URL:
-    "/api/MinistryAdmin/get-all-new-convert-for-ministry-admin",
+    '/api/MinistryAdmin/get-all-new-convert-for-ministry-admin',
 
   //DTI Admins
-  GET_ALL_DTIAdmin_URL: "/api/DTIAdmin/get-new-convant-for-dtiadmin",
+  GET_ALL_DTIAdmin_URL: '/api/DTIAdmin/get-new-convant-for-dtiadmin',
 
   //Analytics Count
-  GET_ALL_SOULS_COUNT_URL: "/api/SuperAdmin/get-all-newconvert-count",
-  GET_ALL_WORKERS_COUNT_URL: "/api/SuperAdmin/get-all-worker-count",
+  GET_ALL_SOULS_COUNT_URL: '/api/SuperAdmin/get-all-newconvert-count',
+  GET_ALL_WORKERS_COUNT_URL: '/api/SuperAdmin/get-all-worker-count',
 
   //Approvals
-  GET_ALL_UNAPPROVED_WORKERS: "/api/SuperAdmin/get-all-unapproved-workers",
-  GET_ALL_DEACTIVATED_WORKERS: "/api/SuperAdmin/get-all-deactivated-workers",
-  APPROVE_A_WORKER: "/api/SuperAdmin/approve-new-worker", //This api takes in an {id}
-  SUSPEND_A_WORKER: "api/SuperAdmin/deactivate-worker-account", //This api takes in an {id}
-  DELETE_A_WORKER: "/api/SuperAdmin/delete-worker-account", //This api takes in an {id}
-  REACTIVATE_A_WORKER: "/api/SuperAdmin/reactivate-worker-account", //This api takes in an {id}
+  GET_ALL_UNAPPROVED_WORKERS: '/api/SuperAdmin/get-all-unapproved-workers',
+  GET_ALL_DEACTIVATED_WORKERS: '/api/SuperAdmin/get-all-deactivated-workers',
+  APPROVE_A_WORKER: '/api/SuperAdmin/approve-new-worker', //This api takes in an {id}
+  SUSPEND_A_WORKER: 'api/SuperAdmin/deactivate-worker-account', //This api takes in an {id}
+  DELETE_A_WORKER: '/api/SuperAdmin/delete-worker-account', //This api takes in an {id}
+  REACTIVATE_A_WORKER: '/api/SuperAdmin/reactivate-worker-account', //This api takes in an {id}
 
   // Profile
-  CHANGEPASSWORD_URL: "/api/v1/Authentication/change-password",
-  UPDATEUSER_URL: "/api/Worker/update-worker-profile",
+  CHANGEPASSWORD_URL: '/api/v1/Authentication/change-password',
+  UPDATEUSER_URL: '/api/Worker/update-worker-profile',
 };
