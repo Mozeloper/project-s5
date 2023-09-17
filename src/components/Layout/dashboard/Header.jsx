78d8bbfa74d0a901dfa78f080df770a772d6302c
king danie <daniedemola@gmail.com>
2023-09-17 01:36:16 +0100

    use the fullname field from the server in the header

--- src/components/Layout/dashboard/Header.jsx
+++ src/components/Layout/dashboard/Header.jsx
@@ -38,7 +38,7 @@ export default function Header({
         onClick={() => toggleDrawer()}
         className="w-[18px] h-[20px] cursor-pointer lg:hidden block"
       />
-      <div className="flex items-center gap-4">
+      <div className="">
         {/* Uncomment this line of code below to show YearJoined and full name on the top right of the dashboard */}
 
         {/* <div className="flex flex-col">
@@ -50,53 +50,63 @@ export default function Header({
             {userObj?.YearJoined}
           </h4>
         </div> */}
-        <div className="w-auto h-auto min-w-[40px] min-h-[40px] font-medium text-xl flex justify-center items-center rounded-full bg-MODAL_BACKGROUND p-2">
-          {`${userObj?.FirstName + userObj?.SurName}`?.charAt(0).toUpperCase() +
-            `${userObj?.SurName + userObj?.FirstName}`?.charAt(
-              `${userObj?.FirstName + userObj?.SurName}`?.indexOf(" ") + 1
-            ).toUpperCase()}
-        </div>
+
         <div
           onClick={() => (isMenuOpen ? handleIconLeave() : handleIconHover())}
           onMouseLeave={handleIconLeave}
-          className="relative"
+          className="relative flex items-center cursor-pointer"
         >
+          <div className="w-auto h-auto min-w-[40px] min-h-[40px] font-medium text-xl flex justify-center items-center rounded-full bg-MODAL_BACKGROUND p-2">
+            {`${userObj?.FirstName + userObj?.SurName}`
+              ?.charAt(0)
+              .toUpperCase() +
+              `${userObj?.SurName + userObj?.FirstName}`
+                ?.charAt(
+                  `${userObj?.FirstName + userObj?.SurName}`?.indexOf(' ') + 1
+                )
+                .toUpperCase()}
+          </div>
           <img
             src={ArrowDown}
             alt="icon"
             loading="lazy"
-            className="h-[29px] w-[29px] cursor-pointer"
+            className="h-[29px] w-[29px]"
           />
           {isMenuOpen && (
             <div className="absolute top-full right-0 w-[290px] h-[240px] bg-white shadow-lg rounded-2xl z-40">
-              <div 
+              <div
                 className="flex gap-4 border-b border-borderColor p-6 cursor-pointer hover:bg-slate-100 rounded-2xl duration-300 ease-in-out"
-                onClick={() => navigate("/profile")}
+                onClick={() => navigate('/profile')}
               >
                 <div className="w-auto h-auto text-white min-w-[40px] min-h-[40px] font-medium text-xl flex justify-center items-center  rounded-full bg-[#38404b] p-2">
-                {`${userObj?.FirstName + userObj?.SurName}`?.charAt(0).toUpperCase() +
-                  `${userObj?.SurName + userObj?.FirstName}`?.charAt(
-                    `${userObj?.FirstName + userObj?.SurName}`?.indexOf(" ") + 1
-                  ).toUpperCase()}
+                  {`${userObj?.FirstName + userObj?.SurName}`
+                    ?.charAt(0)
+                    .toUpperCase() +
+                    `${userObj?.SurName + userObj?.FirstName}`
+                      ?.charAt(
+                        `${userObj?.FirstName + userObj?.SurName}`?.indexOf(
+                          ' '
+                        ) + 1
+                      )
+                      .toUpperCase()}
                 </div>
                 <div className="flex flex-col gap-1 justify-between">
-                  <h3 className="text-textDark_200 font-black text-sm leading-[18px]">
-                    {toPascalCase(userObj?.FirstName)} {toPascalCase(userObj?.SurName)}
+                  <h3 className="text-textDark_200 font-black text-sm leading-[18px] capitalize">
+                    {/* {toPascalCase(userObj?.FirstName)}{' '}
+                    {toPascalCase(userObj?.SurName)} */}
+                    {userObj?.FullName}
                   </h3>
                   <h3 className="text-secondary font-bold text-sm leading-[18px] text-right">
-                    <span className="font-black">Year Joined:</span>{" "}
+                    <span className="font-black">Year Joined:</span>{' '}
                     {userObj?.YearJoined}
                   </h3>
                 </div>
               </div>
-              <div 
+              <div
                 className="border-b border-borderColor p-6 hover:bg-slate-100 rounded-2xl duration-300 ease-in-out cursor-pointer"
-                onClick={() => navigate("/profile")}
+                onClick={() => navigate('/profile')}
               >
-                <div
-                  
-                  className="flex gap-4"
-                >
+                <div className="flex gap-4">
                   <img
                     src={profile}
                     alt="icon"
@@ -108,13 +118,11 @@ export default function Header({
                   </h3>
                 </div>
               </div>
-              <div 
+              <div
                 className="p-6 hover:bg-slate-100 rounded-2xl duration-300 ease-in-out cursor-pointer"
                 onClick={() => setLogoutConfirmation(true)}
               >
-                <div
-                  className="flex gap-4"
-                >
+                <div className="flex gap-4">
                   <FiLogOut className="h-[16px] w-[14px] cursor-pointer text-[#8A9099]" />
                   <h3 className="text-textDark_200 font-normal text-sm leading-5">
                     Logout
