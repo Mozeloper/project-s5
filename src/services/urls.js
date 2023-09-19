2509828dba082be9d743b302833b3ce5fed03b31
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-09-19 13:19:20 +0100

    revert to old url styling

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,66 +1,66 @@
 export const appUrls = {
   //Authentication
-  LOGIN_URL: '/Authentication/login',
-  REGISTER: '/Authentication/register',
-  REFRESHTOKEN_URL: '/Authentication/refresh-token',
-  FORGETPASSWORD_URL: '/Authentication/forget-password',
-  RESETPASSWORD_URL: '/Authentication/reset-password',
+  LOGIN_URL: '/api/v1/Authentication/login',
+  REGISTER: '/api/v1/Authentication/register',
+  REFRESHTOKEN_URL: '/api/v1/Authentication/refresh-token',
+  FORGETPASSWORD_URL: '/api/v1/Authentication/forget-password',
+  RESETPASSWORD_URL: '/api/v1/Authentication/reset-password',
 
   //Department
-  GETCHURCHDEPT: '/Entity/get-church-department',
+  GETCHURCHDEPT: '/api/v1/Entity/get-church-department',
 
   //Super Admins
-  GETALLWORKERS: '/SuperAdmin/get-all-workers', //
-  GET_ALL_SUPERADMINS: '/SuperAdmin/get-all-admin',
-  GET_ADMIN_DETAILS: '/SuperAdmin/get-admin-by', //get worker by id
-  GET_ALL_New_Converts_URL: '/SuperAdmin/get-all-new-converts',
-  GET_WORKER_DETAILS: '/SuperAdmin/get-worker', //get worker by id
-  ADD_ROLE_TO_USER: '/SuperAdmin/Add-user-role', //accepts id & role as query  eg ?userId=1&roles=User
-  REMOVE_ADMIN_FROM_ROLE: '/SuperAdmin/Remove-user-role', //remove a role from an admin, accepts the admin's id and the role as a query param eg /ADMIN_ID?roles=User
+  GETALLWORKERS: '/api/v1/SuperAdmin/get-all-workers', //
+  GET_ALL_SUPERADMINS: '/api/v1/SuperAdmin/get-all-admin',
+  GET_ADMIN_DETAILS: '/api/v1/SuperAdmin/get-admin-by', //get worker by id
+  GET_ALL_New_Converts_URL: '/api/v1/SuperAdmin/get-all-new-converts',
+  GET_WORKER_DETAILS: '/api/v1/SuperAdmin/get-worker', //get worker by id
+  ADD_ROLE_TO_USER: '/api/v1/SuperAdmin/Add-user-role', //accepts id & role as query  eg ?userId=1&roles=User
+  REMOVE_ADMIN_FROM_ROLE: '/api/v1/SuperAdmin/Remove-user-role', //remove a role from an admin, accepts the admin's id and the role as a query param eg /ADMIN_ID?roles=User
 
   //Worker
-  GETSINGLEWORKERDETAILS_URL: '/Worker/get-worker-profile', //user's pofile
+  GETSINGLEWORKERDETAILS_URL: '/api/v1/Worker/get-worker-profile', //user's pofile
 
   //New converts (soul)
   GET_ALL_New_Convert:
-    '/NewConvertAdmin/get-all-new-convants-for-newconvertadmin',
-  GET_CONVERT_DETAILS: '/NewConvert/get-new-converts', //get soul by id
-  GET_ALL_SOULS_UNDER_A_WORKER: '/NewConvert/get-new-converts-by-worker', //accepts worker's id
-  DELETE_A_CONVERT: '/NewConvert/delete-new-convert', //delete soul by id
-  SUSPEND_A_CONVERT: '/NewConvert/deactivate-new-convert', //suspend soul by id
-  REACTIVATE_A_CONVERT: '/NewConvert/reactivate-new-convert', //reactivate soul by id
+    '/api/v1/NewConvertAdmin/get-all-new-convants-for-newconvertadmin',
+  GET_CONVERT_DETAILS: '/api/v1/NewConvert/get-new-converts', //get soul by id
+  GET_ALL_SOULS_UNDER_A_WORKER: '/api/v1/NewConvert/get-new-converts-by-worker', //accepts worker's id
+  DELETE_A_CONVERT: '/api/v1/NewConvert/delete-new-convert', //delete soul by id
+  SUSPEND_A_CONVERT: '/api/v1/NewConvert/deactivate-new-convert', //suspend soul by id
+  REACTIVATE_A_CONVERT: '/api/v1/NewConvert/reactivate-new-convert', //reactivate soul by id
 
   //Ministry
   GET_ALL_MINISTRY_URL:
-    '/MinistryAdmin/get-all-new-convert-for-ministry-admin',
+    '/api/v1/MinistryAdmin/get-all-new-convert-for-ministry-admin',
 
   //DTI Admins
-  GET_ALL_DTIAdmin_URL: '/DTIAdmin/get-new-convant-for-dtiadmin',
+  GET_ALL_DTIAdmin_URL: '/api/v1/DTIAdmin/get-new-convant-for-dtiadmin',
 
   //Analytics Count
-  GET_ALL_SOULS_COUNT_URL: '/SuperAdmin/get-all-newconvert-count',
-  GET_ALL_WORKERS_COUNT_URL: '/SuperAdmin/get-all-worker-count',
+  GET_ALL_SOULS_COUNT_URL: '/api/v1/SuperAdmin/get-all-newconvert-count',
+  GET_ALL_WORKERS_COUNT_URL: '/api/v1/SuperAdmin/get-all-worker-count',
   ADMINS_DASHBOARD_ANALYTICS:
-    '/Analytics/Admin-dashboard/retrieve-newconverts-analytics',
+    '/api/v1/Analytics/Admin-dashboard/retrieve-newconverts-analytics',
   NEWCONVERT_DASHBOARD_ANALYTICS:
-    '/Analytics/newconvertAdmin-dashboard/retrieve-newconverts-analytics',
+    '/api/v1/Analytics/newconvertAdmin-dashboard/retrieve-newconverts-analytics',
   DTI_ADMINS_DASHBOARD_ANALYTICS:
-    '/Analytics/dtiadmin-dashboard/retrieve-newconverts-analytics',
+    '/api/v1/Analytics/dtiadmin-dashboard/retrieve-newconverts-analytics',
   MINISTRY_ADMINS_DASHBOARD_ANALYTICS:
-    '/Analytics/mininstryadmin-dashboard/retrieve-newconverts-analytics',
+    '/api/v1/Analytics/mininstryadmin-dashboard/retrieve-newconverts-analytics',
   PERSONAL_ANALYTICS_BY_ID:
-    '/Analytics/worker-dashboard/retrieve-newconverts-analytics',
+    '/api/v1/Analytics/worker-dashboard/retrieve-newconverts-analytics',
     
   //Approvals
-  GET_ALL_UNAPPROVED_WORKERS: '/Approval/get-all-unapproved-workers',
-  GET_UNAPPROVED_WORKERDETAILS: '/Approval/get-unapproved-worker', //This api takes in an {workerId}
-  GET_ALL_DEACTIVATED_WORKERS: '/SuperAdmin/get-all-deactivated-workers',
-  APPROVE_A_WORKER: '/SuperAdmin/approve-new-worker', //This api takes in an {id}
+  GET_ALL_UNAPPROVED_WORKERS: '/api/v1/Approval/get-all-unapproved-workers',
+  GET_UNAPPROVED_WORKERDETAILS: '/api/v1/Approval/get-unapproved-worker', //This api takes in an {workerId}
+  GET_ALL_DEACTIVATED_WORKERS: '/api/v1/SuperAdmin/get-all-deactivated-workers',
+  APPROVE_A_WORKER: '/api/v1/SuperAdmin/approve-new-worker', //This api takes in an {id}
   SUSPEND_A_WORKER: 'api/SuperAdmin/deactivate-worker-account', //This api takes in an {id}
-  DELETE_A_WORKER: '/SuperAdmin/delete-worker-account', //This api takes in an {id}
-  REACTIVATE_A_WORKER: '/SuperAdmin/reactivate-worker-account', //This api takes in an {id}
+  DELETE_A_WORKER: '/api/v1/SuperAdmin/delete-worker-account', //This api takes in an {id}
+  REACTIVATE_A_WORKER: '/api/v1/SuperAdmin/reactivate-worker-account', //This api takes in an {id}
 
   // Profile
-  CHANGEPASSWORD_URL: '/v1/Authentication/change-password',
-  UPDATEUSER_URL: '/Worker/update-worker-profile',
+  CHANGEPASSWORD_URL: '/api/v1/v1/Authentication/change-password',
+  UPDATEUSER_URL: '/api/v1/Worker/update-worker-profile',
 };
