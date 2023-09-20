0d851a77d854cdacd08fc42068f49c20a18ba83c
king danie <daniedemola@gmail.com>
2023-09-20 16:51:22 +0100

    added useFetchAllNewConvertDynamic function

--- src/hooks/useFetchNewConvert.js
+++ src/hooks/useFetchNewConvert.js
@@ -1,23 +1,57 @@
-import { useQuery } from "react-query";
-import { getAllNewConvert } from "../services/souls";
-import { getASoul } from "../services/details(id).api";
+import { useQuery } from 'react-query';
+import { getAllNewConvert, getSoulsUnderWorker } from '../services/souls';
+import { getASoul } from '../services/details(id).api';
 
 export function useFetchAllNewConvert({ pageNumber }) {
-    const NewConvert = useQuery([`NewBeliever`, pageNumber], async () => await getAllNewConvert({ pageNumber }), {
+  const NewConvert = useQuery(
+    [`NewBeliever`, pageNumber],
+    async () => await getAllNewConvert({ pageNumber }),
+    {
+      staleTime: 360000,
+      enabled: !!pageNumber,
+      keepPreviousData: true,
+    }
+  );
+  return NewConvert;
+}
+export function useFetchAllNewConvertDynamic({
+  workerId,
+  isAdmin,
+  pageNumber,
+}) {
+  console.log(`is this an admin request? ${isAdmin}`);
+  if (isAdmin == true) {
+    const NewConvert = useQuery(
+      [`NewBeliever`, pageNumber],
+      async () => await getAllNewConvert({ pageNumber }),
+      {
         staleTime: 360000,
         enabled: !!pageNumber,
         keepPreviousData: true,
-    });
-    return NewConvert
+      }
+    );
+    return NewConvert;
+  } else {
+    const NewConvert = useQuery(
+      [`NewBeliever`, pageNumber],
+      async () => await getSoulsUnderWorker({ workerId, pageNumber }),
+      {
+        staleTime: 360000,
+        enabled: !!pageNumber,
+        keepPreviousData: true,
+      }
+    );
+    return NewConvert;
+  }
 }
 
 export function useSoulDetails({ soulId }) {
-    // console.log('singleSoul soulId', soulId);
-    const singleSoul = useQuery({
-        queryKey: ['soul', soulId],
-        queryFn: async () => await getASoul(soulId),
-        staleTime: 360000,
-        enabled: !!soulId, //Only run this function if soulId is available
-    })
-    return singleSoul;
+  // console.log('singleSoul soulId', soulId);
+  const singleSoul = useQuery({
+    queryKey: ['soul', soulId],
+    queryFn: async () => await getASoul(soulId),
+    staleTime: 360000,
+    enabled: !!soulId, //Only run this function if soulId is available
+  });
+  return singleSoul;
 }
