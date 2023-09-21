f4078c188cbea36c2b41e7885e3d17cbc762a3a9
king danie <daniedemola@gmail.com>
2023-09-21 18:19:00 +0100

    slight modifications to ministry hook

--- src/hooks/useFetchMinistry.js
+++ src/hooks/useFetchMinistry.js
@@ -2,11 +2,15 @@ import { useQuery } from "react-query"
 import { getAllMinistryAdmins } from "../services/admins.api";
 
 export function useFetchMinistry({ pageNumber, pageSize }) {
-    const Ministry = useQuery([`Ministry`, pageNumber], async () => await getAllMinistryAdmins({ pageNumber, pageSize }), {
+    const Ministers = useQuery(
+      [`GetAllMinisters`, pageNumber],
+      async () => await getAllMinistryAdmins({ pageNumber, pageSize }),
+      {
         staleTime: 360000,
         enabled: !!pageNumber,
-        keepPreviousData: true
-    });
+        keepPreviousData: true,
+      }
+    );
 
-    return Ministry
+    return Ministers;
 }
