b9bd0d41413aa9e1ea1c80dabaadd7de13f3016d
king danie <daniedemola@gmail.com>
2023-10-01 22:32:29 +0100

    fixed new converts analytics on new converts page

--- src/pages/dashboard/NewConvert/index.jsx
+++ src/pages/dashboard/NewConvert/index.jsx
@@ -8,25 +8,13 @@ import { useFetchNewConvertDashboardAnalytics } from '../../../hooks/useFetchAna
 //Todo - 2. Replace useEffect with react query for data fetching
 //Todo - 3. Ensure that the "get-all-new-converts" endpoint is now working and not returning 500 then uncomment the code in the SoulsTable
 export default function NewConvert() {
-  const workerId = JSON.parse(sessionStorage.getItem('userObj')).Id;
-  console.log(workerId);
   const {
     data: NewConvertDashboardAnalytics,
     isError,
     isLoading,
-  } = useFetchNewConvertDashboardAnalytics(workerId);
-  const ChartDatas = [
-    {
-      name: '2022',
-      data: [20, 90, 50, 30, 40, 50, 70, 30, 60, 33, 52, 89],
-    },
-    {
-      name: '2023',
-      data: [10, 20, 40, 50, 70, 30, 60, 73, 60, 20, 40, 50],
-    },
-  ];
+  } = useFetchNewConvertDashboardAnalytics();
 
-  const summeryTitle = ['Souls'];
+
   return (
     <div className="flex flex-col gap-y-6">
       <PageTitle title="New Believers Stage" />
