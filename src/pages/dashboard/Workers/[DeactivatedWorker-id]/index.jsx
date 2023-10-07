fdd07772a2d2d48a48a3906ed080a75aa76345ac
king danie <daniedemola@gmail.com>
2023-10-07 13:20:29 +0100

    added the deactivatedworker details comp and route

--- src/pages/dashboard/Workers/[DeactivatedWorker-id]/index.jsx
+++ src/pages/dashboard/Workers/[DeactivatedWorker-id]/index.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import DetailsByIdScreen from '../../../../components/UI/Details Screen/DetailsByIdScreen';
 import { useDeactivatedWorkerDetails } from '../../../../hooks/useWorkers';
 import { useFetchPersonalAnalytics } from '../../../../hooks/useFetchAnalytics';
-
+import DeactivatedWorkerByIdDetailsScreen from '../../../../components/UI/DeactivatedWorkerByIdDetailsScreen/DeactivatedWorkerByIdDetailsScreen';
 
 export default function Worker() {
   const { workerId } = useParams();
@@ -16,7 +16,14 @@ export default function Worker() {
 
   return (
     <div>
-      <DetailsByIdScreen personalAnalyticsDatas={personalAnalyticsDatas && personalAnalyticsDatas?.Data} data={!isLoading && workerInfo} loading={isLoading} notFound={!workerInfo && isError} />
+      <DeactivatedWorkerByIdDetailsScreen
+        personalAnalyticsDatas={
+          personalAnalyticsDatas && personalAnalyticsDatas?.Data
+        }
+        data={!isLoading && workerInfo}
+        loading={isLoading}
+        notFound={!workerInfo && isError}
+      />
     </div>
   );
 }
