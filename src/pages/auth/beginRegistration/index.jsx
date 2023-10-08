28bcfe7bae9b7ccf35270f6b965e27f1b42c5b34
king danie <daniedemola@gmail.com>
2023-10-08 18:51:55 +0100

    changed wording on registration page

--- src/pages/auth/beginRegistration/index.jsx
+++ src/pages/auth/beginRegistration/index.jsx
@@ -6,24 +6,25 @@ import Button from "../../../components/Button";
 export default function BeginRegistration() {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-full flex md:items-center md:pt-0 pt-16 xl:px-36 lg:px-16 md:px-4 px-2">
-      <div className="2xl:w-[65%] lg:w-[100%] w-full flex flex-col gap-4 cursor-pointer mx-auto">
+    <div className="w-full h-full flex md:items-center md:pt-0 pt-10 xl:px-36 lg:px-16 md:px-4 px-2">
+      <div className="2xl:w-[65%] lg:w-[100%] w-full flex flex-col gap-10 md:gap-20 cursor-pointer mx-auto">
         <img
           onClick={() => navigate("/")}
           src={logo}
           alt="logo"
           className="w-[300px]"
         />
+        <div>
         <h2 className="text-4xl font-black md:text-secondary text-white leading-9">
-          Welcome to Operation 5 <span className="text-primary">S</span> portal
+          Welcome to Operation 5<span className="text-primary">S</span> portal
         </h2>
-        <p className="md:text-textDark_200 text-white font-normal text-lg leading-7">
-          Please be informed that to successfully create an account, you need to
-          be a member of the TPHOL'S workforce.
+        <p className="md:text-textDark_200 text-white font-normal text-lg leading-7 pt-5">
+          Please be advised that only TPHOL'S members are allowed access to this portal.
         </p>
-        <p className="md:text-textDark_200 text-white font-normal text-lg mb-3 leading-7">
+        </div>
+        {/* <p className="md:text-textDark_200 text-white font-normal text-lg mb-3 leading-7">
           Kindly click the button below to begin the registration process.
-        </p>
+        </p> */}
         <div className="flex md:flex-row flex-col gap-3">
           <Button
             onClick={() => navigate("/sign-up")}
