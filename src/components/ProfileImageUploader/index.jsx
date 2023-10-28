c5f8e49e6356c220ac359c32d5e412a5f047abad
king danie <daniedemola@gmail.com>
2023-10-28 14:20:42 +0100

    updated profileImage comp

--- src/components/ProfileImageUploader/index.jsx
+++ src/components/ProfileImageUploader/index.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import ModalPopup from './ModalPopup'; // Import your modal component
+import Button from '../Button';
+import { uploadProfileImage } from '../../services/worker.api';
+import { useModalToggle } from '../../context/ConfirmationModal.context';
 
-export default function ProfileImageUploader({ imageUrl, onImageChange }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newImageUrl, setNewImageUrl] = useState(imageUrl || '');
+export default function ProfileImageUploader({ imageUrl }) {
+  const [newImageUrl, setNewImageUrl] = useState('');
+  const [ImageUrl, setImageUrl] = useState(imageUrl || '');
+  const [isLoading, setIsLoading] = useState(false);
+  const { closeModal } = useModalToggle();
+
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     accept: 'image/*', // Allow only image files
     multiple: false, // Allow only one file at a time
@@ -12,7 +17,9 @@ export default function ProfileImageUploader({ imageUrl, onImageChange }) {
       // Handle the dropped file
       if (acceptedFiles.length > 0) {
         const file = acceptedFiles[0];
-        setNewImageUrl(URL.createObjectURL(file));
+        console.log(acceptedFiles[0]);
+        setImageUrl(URL.createObjectURL(file));
+        setNewImageUrl(file);
       }
     },
   });
@@ -20,40 +27,54 @@ export default function ProfileImageUploader({ imageUrl, onImageChange }) {
   const handleSaveImage = () => {
     if (newImageUrl) {
       // Send the new image to the server or perform any desired action
-      onImageChange(newImageUrl);
-      setIsModalOpen(false);
+      console.log(newImageUrl);
+      setIsLoading(true);
+
+      //   const formData = new FormData();
+      //   formData.append('file', newImageUrl);
+
+      // You can add additional data to the form if needed
+      //formData.append('additionalData', 'someValue');
+
+      try {
+        const uploadImg = uploadProfileImage(newImageUrl);
+        if (uploadImg?.status === 200) {
+          closeModal();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
 
   return (
     <>
-      <img
-        src={newImageUrl || imageUrl}
-        alt="Profile"
-        onClick={() => setIsModalOpen(true)}
-        style={{ cursor: 'pointer' }}
-      />
-
-      {/* This is your modal component to display the uploader */}
-      <ModalPopup isOpen={isModalOpen}>
-        <div className="p-4">
-          <h3>Upload New Profile Image</h3>
-          <div {...getRootProps()} style={dropzoneStyles}>
-            <input {...getInputProps()} />
-            <p>Drag & drop an image here, or click to select one</p>
-          </div>
-          {newImageUrl && (
-            <div>
-              <img
-                src={newImageUrl}
-                alt="Preview"
-                style={{ maxWidth: '100%', maxHeight: '200px' }}
-              />
-            </div>
-          )}
-          <button onClick={handleSaveImage}>Submit</button>
+      <div className="p-4 flex flex-col gap-10">
+        <h3 className="font-bold text-primary">Manage Profile Image</h3>
+        <div {...getRootProps()} style={dropzoneStyles}>
+          <input {...getInputProps()} />
+          <p>Drag & drop an image here, or click to select one</p>
         </div>
-      </ModalPopup>
+        {ImageUrl && (
+          <div>
+            <img
+              src={ImageUrl}
+              alt="Preview"
+              style={{ maxWidth: '100%', maxHeight: '200px' }}
+              className="rounded"
+            />
+          </div>
+        )}
+
+        <Button
+          onClick={handleSaveImage}
+          isLoading={isLoading}
+          title="Upload Image"
+          type="submit"
+        />
+      </div>
     </>
   );
 }
